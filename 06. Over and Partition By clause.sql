857e615ff6165d246150ba8bbee7f564fd650148
Mayank Birkhani <33277205+MayankBirkhani@users.noreply.github.com>
2023-01-07 00:39:26 +0530

    Add files via upload

---
+++ 06. Over and Partition By clause.sql
@@ -0,0 +1,8 @@
+/* List Firstname, Lastname and Find how many people are from each location and avg salary at each location  */
+
+-- select * from employee;
+
+select firstname, lastname , location,
+count(location) OVER (partition by LOCATION) AS total ,
+avg(salary) OVER (partition by LOCATION) AS Average
+from employee;
